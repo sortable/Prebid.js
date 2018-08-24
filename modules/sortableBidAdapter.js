491f65ef0a4df26e22c9f35e572983be33c3455d
Meng <EdmonL@users.noreply.github.com>
2018-08-24 10:16:41 -0400

    SOR-233 Sortable bid adapter supports passing OpenRTB bidder extension to Prebid.js users (#9)

--- modules/sortableBidAdapter.js
+++ modules/sortableBidAdapter.js
@@ -123,6 +123,9 @@ export const spec = {
           } else if (bid.nurl) {
             bidObj.adUrl = bid.nurl;
           }
+          if (bid.ext) {
+            bidObj[BIDDER_CODE] = bid.ext;
+          }
           sortableBids.push(bidObj);
         });
       });
